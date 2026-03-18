023ebb48a1be5f9bc04c564639220d459aa12fb0
tmackins1 <267549257+tmackins1@users.noreply.github.com>
2026-03-17 21:46:20 -0400

    overlapping images v1

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Hello, here is a website");
+
+// Scroll animation for project images
+const section = document.querySelector(".image-wrap");
+
+const observer = new IntersectionObserver((entries) => {
+  if (entries[0].isIntersecting) {
+    section.classList.add("animate");
+  }
+});
+
+observer.observe(section);
